63107b40732a908fb4c019d65c92542fc9a5c1e5
SamiFraca <fracasami@gmail.com>
2024-07-02 19:44:46 +0200

    Update main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 import { WeatherDataPanel } from "./components/weather-data-panel";
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <WeatherDataPanel />
-    </main>
-  );
+  return <WeatherDataPanel />;
 }
